2836150e5f6a3736de183757b563f321b21b6d6d
Glippy <jonathanong@iMac.local>
2020-12-02 17:09:02 +0800

    created the tab

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import CounterText from './components/CounterText';
+import Accordian from './components/Accordian'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function HomeScreen() {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>Home!</Text>
+		</View>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function EventScreen() {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>Event!</Text>
+      <CounterText color="lightgrey" fontSize={10}>Hello</CounterText>
+     <CounterText color="lightblue" fontSize={30}>Hello</CounterText>
+     <CounterText color="blue" fontSize={60}>Hello</CounterText>
+     <CounterText color="navy" fontSize={90}>Hello</CounterText>
+		</View>
+	);
+}
+
+function ContactScreen() {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>Contact!</Text>
+		</View>
+	);
+}
+
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+	return (
+		<NavigationContainer>
+			<Tab.Navigator
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({ focused, color, size }) => {
+						let iconName;
+
+						if (route.name === 'Home') {
+							iconName = focused ? 'ios-home' : 'ios-home';
+						} else if (route.name === 'Event') {
+							iconName = focused ? 'ios-easel' : 'ios-easel';
+						} else if (route.name === 'Contact') {
+							iconName = focused ? 'ios-paw' : 'ios-paw';
+						}
+
+						// You can return any component that you like here!
+						return <Ionicons name={iconName} size={size} color={color} />;
+					}
+				})}
+				tabBarOptions={{
+					activeTintColor: 'tomato',
+					inactiveTintColor: 'gray'
+				}}
+			>
+				<Tab.Screen name="Home" component={HomeScreen} />
+				<Tab.Screen name="Event" component={EventScreen} />
+				<Tab.Screen name="Contact" component={ContactScreen} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
+}
